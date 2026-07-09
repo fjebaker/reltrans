13309c61bfc6f0706f4a12865b2df451ce3648fa
fjebaker <fergus@cosroe.com>
2026-07-09 12:54:17 +0100

    test: adjust tolerances on zero spin

--- test/test_edgecases.py
+++ test/test_edgecases.py
@@ -12,7 +12,10 @@ def test_dcp_no_spin(reltrans, assert_snapshot):
     reltrans.reset()
     energy = np.logspace(np.log10(0.1), np.log10(100), 501)
     output = reltrans.dcp(energy, DCP_Parameters(a=0))
-    assert_snapshot(output, domain = energy[0:-1], **plot_spectral_kwargs)
+    # TODO: kerrz does not have directly `0` spin implemented yet, so it is
+    # approximated with spin=1e-3. The uncertainly on the calculation is prone
+    # to numerical slips and therefore a greater error.
+    assert_snapshot(output, domain = energy[0:-1], **plot_spectral_kwargs, rtol = 0.02)
 
 def test_dcp_max_spin(reltrans, assert_snapshot):
     reltrans.reset()
